53b2afe195e9ac9992143f7c044623cd8382963b
Erwin Andres <eandres@stratpoint.com>
2023-10-24 16:49:57 +0800

    feat(utils/sendEmail): add email sending feature

---
+++ module2/temp-notif/util/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (data) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST || "smtp.forwardemail.net",
+    port: process.env.SMTP_PORT || 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER || "REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM",
+      pass: process.env.SMTP_PASS || "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
+    },
+  })
+
+  const mailOptions = {
+    from: 'youremail@gmail.com',
+    to: process.env.EMAIL_TO || 'myfriend@yahoo.com',
+    subject: 'Temperature Alert!',
+    text: `Temperature exceeded the threshold! ${data}`
+  }
+
+  transporter.sendMail(mailOptions)
+}
+
+
