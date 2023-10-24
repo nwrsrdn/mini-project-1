a087d4fb6c406e8d7432c82ae45191e0371c29e5
Erwin Andres <eandres@stratpoint.com>
2023-10-24 16:47:43 +0800

    feat(validatorMiddleware): add validation handling

---
+++ module2/temp-notif/middleware/validatorMiddleware.js
@@ -0,0 +1,11 @@
+const { validationResult } = require('express-validator')
+
+const validateRule = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  next()
+}
+
+module.exports = validateRule
