ffbe1b19e5b2b1b3fc16eb7a512178c5afb4fef2
Erwin Andres <eandres@stratpoint.com>
2023-10-24 14:05:42 +0800

    feat(config/loggerConfig): add timestamp on logger

--- module2/temp-notif/config/loggerConfig.js
+++ module2/temp-notif/config/loggerConfig.js
@@ -1,6 +1,14 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
   transports: [
     new transports.Console(),
     new transports.File({ filename: 'combined.log' })
@@ -8,9 +16,9 @@ const logger = createLogger({
 })
 
 if (process.env.NODE_ENV !== 'production') {
-    logger.add(new transports.Console({
-        format: format.simple(),
-    }));
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
 }
 
 module.exports = logger;
